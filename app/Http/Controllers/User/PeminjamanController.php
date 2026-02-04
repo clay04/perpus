1cc583a253ce1df362610c0d26b0e64fee01b342
clay04 <s22110530@student.unklab.ac.id>
2026-02-05 01:29:10 +0800

    fixing peminjaman

--- app/Http/Controllers/User/PeminjamanController.php
+++ app/Http/Controllers/User/PeminjamanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Book;
 use App\Models\Peminjaman;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PeminjamanController extends Controller
 {
@@ -24,21 +25,45 @@ class PeminjamanController extends Controller
             'tanggal_kembali' => 'required|date|after:tanggal_pinjam',
         ]);
 
+        $exists = Peminjaman::where('user_id', auth()->id())
+            ->where('book_id', $request->book_id)
+            ->where('status', 'dipinjam')
+            ->exists();
+
+        if ($exists) {
+            return back()->withErrors('Buku ini belum dikembalikan');
+        }
+
         $book = Book::findOrFail($request->book_id);
 
         if ($book->stok < 1) {
             return back()->withErrors('Stok buku habis');
         }
 
-        Peminjaman::create([
-            'user_id' => auth()->id(),
-            'book_id' => $book->id,
-            'tanggal_pinjam' => $request->tanggal_pinjam,
-            'tanggal_kembali' => $request->tanggal_kembali,
-            'status' => 'dipinjam',
-        ]);
+        try {
+        // 2️⃣ Transaction + Lock
+            DB::transaction(function () use ($request) {
 
-        $book->decrement('stok');
+                $book = Book::lockForUpdate()->findOrFail($request->book_id);
+
+                if ($book->stok < 1) {
+                    throw new \Exception('Stok buku habis');
+                }
+
+                Peminjaman::create([
+                    'user_id' => auth()->id(),
+                    'book_id' => $book->id,
+                    'tanggal_pinjam' => $request->tanggal_pinjam,
+                    'tanggal_kembali' => $request->tanggal_kembali,
+                    'status' => 'dipinjam',
+                ]);
+
+                $book->decrement('stok');
+            });
+
+        } catch (\Exception $e) {
+            return back()->withErrors($e->getMessage());
+        }
 
         return back()->with('success', 'Buku berhasil dipinjam');
     }

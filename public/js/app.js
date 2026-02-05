7705370d5a1870ff4ca0c247beed0155b661b2fb
clay04 <s22110530@student.unklab.ac.id>
2026-02-05 22:06:07 +0800

    test modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,6 @@
 console.log('App.js is loaded');
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.delete-form').forEach(form => {
-        form.addEventListener('submit', function (e) {
-            if (!confirm('Yakin hapus buku ini?')) {
-                e.preventDefault();
-            }
-        });
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.delete-form-user').forEach(form => {
-        form.addEventListener('submit', function (e) {
-            if (!confirm('Yakin hapus buku ini?')) {
-                e.preventDefault();
-            }
-        });
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
     const deleteModal = new bootstrap.Modal(
         document.getElementById('deleteModal')
     );

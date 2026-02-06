790d373fc4c59beeed216e8aab9d6a5c363eb53a
clay04 <s22110530@student.unklab.ac.id>
2026-02-06 13:51:33 +0800

    .

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,19 @@
 console.log('App.js is loaded');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const modalEl = document.getElementById('deleteModal');
-    if (!modalEl) return;
+// document.addEventListener('DOMContentLoaded', () => {
+//     const modalEl = document.getElementById('deleteModal');
+//     if (!modalEl) return;
 
-    const deleteModal = new bootstrap.Modal(modalEl);
-    const deleteForm  = document.getElementById('deleteForm');
-    const deleteTitle = document.getElementById('deleteTitle');
+//     const deleteModal = new bootstrap.Modal(modalEl);
+//     const deleteForm  = document.getElementById('deleteForm');
+//     const deleteTitle = document.getElementById('deleteTitle');
 
-    document.querySelectorAll('.btn-delete').forEach(btn => {
-        btn.addEventListener('click', () => {
-            deleteForm.action = btn.dataset.action;
-            deleteTitle.textContent = btn.dataset.title;
-            deleteModal.show();
-        });
-    });
-});
+//     document.querySelectorAll('.btn-delete').forEach(btn => {
+//         btn.addEventListener('click', () => {
+//             deleteForm.action = btn.dataset.action;
+//             deleteTitle.textContent = btn.dataset.title;
+//             deleteModal.show();
+//         });
+//     });
+// });
 

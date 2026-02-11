f6609d02d5f56ee25d5c73a451bace39569c168d
clay04 <s22110530@student.unklab.ac.id>
2026-02-11 22:10:03 +0800

    ,

--- resources/views/pages/admin/books/show.blade.php
+++ resources/views/pages/admin/books/show.blade.php
@@ -77,15 +77,18 @@
     </div>
 
     @if (isset($fileUrl))
-    
+        <div class="card shadow-sm mt-4">
+            <div class="card-header">
+                <strong>Preview Buku ({{ $previewPages }}) halaman</strong>
+            </div>
+            <div class="card-body">
+                <div id="pdf-container"
+                    data-url="{{ $fileUrl }}"
+                    data-limit="{{ $previewPages }}">
+                </div>
+            </div>
+        </div>
     @endif
-    <div class="card shadow-sm mt-4">
-        <div class="card-header">
-            <strong>Privew Buku ({{ $previewPages }}) halaman</strong>
-        </div>
-        <div class="card-body">
-            <div id="pdf-container"></div>
-        </div>
-    </div>
+
 </div>
 @endsection
